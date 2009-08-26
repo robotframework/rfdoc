f6f24f137f1674080ca4a84bbaa86451cf2cbdeb
Janne Harkonen <jth@reaktor.fi>
2009-08-26 10:09:37 +0300

    Whitespace cleanup, test commit with mercurial

--- trunk/setup.py
+++ trunk/setup.py
@@ -10,19 +10,19 @@ DESCRIPTION = 'A web based system for storing and searching Robot Framework ' \
 
 def main():
     setup(
-        name         = 'rfdoc', 
+        name         = 'rfdoc',
         version      = VERSION,
         description  = DESCRIPTION,
         author       = 'Robot Framework Developers',
         author_email = 'robotframework-devel@googlegroups.com',
-        url          = 'http://code.google.com/p/rfdoc/', 
+        url          = 'http://code.google.com/p/rfdoc/',
         license      = 'Apache License 2.0',
         package_dir  = {'rfdoc': 'src/rfdoc'},
-        packages     = ['rfdoc', 'rfdoc.rfdocapp', 'rfdoc.rfdocapp.views', 
+        packages     = ['rfdoc', 'rfdoc.rfdocapp', 'rfdoc.rfdocapp.views',
                         'rfdoc.rfdocapp.templatetags', 'rfdoc.rfdocapp.utils'],
         package_data = {'rfdoc': ['*.tmpl', 'rfdocapp/templates/*.html']}
         )
-    
+
 
 if __name__ == "__main__":
     main()
