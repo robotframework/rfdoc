30b2e5d28db547985bba5813e5d7f402870d7642
Juha Rantanen <Juha Rantanen@localhost>
2009-10-14 13:51:05 +0300

    Some more cleaning.

--- atest/lib/NameParser.py
+++ atest/lib/NameParser.py
@@ -2,3 +2,6 @@ def get_names(namestr):
     if namestr and namestr[0] == namestr[-1] == '"':
         namestr = namestr[1:-1]
     return namestr.replace(' and ', ', ').split('", "')
+
+def get_library_name(library_name):
+    return library_name.split()[0]
