20f73ca8c05b83af3559a05f855a7e85fd5767b6
Anssi Syrj?salo <anssi.syrjasalo@eficode.com>
2013-10-07 15:18:10 +0300

    Acceptance tests to run on Selenium2Library

--- atest/atest_settings.py
+++ atest/atest_settings.py
@@ -1,43 +1,63 @@
 # Django settings for RFDoc project acceptance tests.
 
-from os.path import abspath, dirname, join
 import sys
+from os.path import abspath, dirname, join
+
 
 _SRCDIR = join(dirname(abspath(__file__)), '..', 'src' )
 _RFDOCDIR = join(_SRCDIR, 'rfdoc')
 sys.path = [_SRCDIR, _RFDOCDIR] + sys.path
 
-DEBUG = False
+DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
-DATABASE_ENGINE = 'sqlite3' 
-DATABASE_NAME = join(dirname(__file__), 'results', 'rfdoc.db')
 TIME_ZONE = 'Europe/Helsinki'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': join(dirname(__file__), 'results', 'rfdoc.db')
+    }
+}
+
+ALLOWED_HOSTS = ['localhost']
 
 SITE_ID = 1
 
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.load_template_source',
-    'django.template.loaders.app_directories.load_template_source',
-)
+# If you set this to False, Django will make some optimizations so as not
+# to load the internationalization machinery.
+USE_I18N = False
+
+STATIC_URL = '/static/'
+
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = 'pmst_958#g=ks#i+(ci!pnf5=1b73@nf(c%h8)p&sc7wongki6'
+
+TEMPLATE_LOADERS = [
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader'
+]
 
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware'
 )
 
 ROOT_URLCONF = 'rfdoc.urls'
 
 TEMPLATE_DIRS = (
-    join(_SRCDIR, 'rfdocapp', 'templates').replace('\\', '/'),
+    join(_RFDOCDIR, 'rfdocapp', 'templates').replace('\\', '/'),
 )
 
 INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.admin',
     'django.contrib.contenttypes',
+    'django.contrib.messages',
     'django.contrib.sessions',
     'django.contrib.sites',
+    'django.contrib.staticfiles',
     'rfdoc.rfdocapp'
 )
