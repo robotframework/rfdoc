407f65a08f611b51f69bdf69bf8120ec2396a9b2
Anssi Syrj?salo <anssi.syrjasalo@eficode.com>
2013-12-30 19:17:00 +0200

    WSGI file to add site-packages to sys.path
    Update issue 15
    
    WSGI file now adds all the site-packages to the module search path to avoid Django import errors.

--- src/rfdoc/rfdoc_wsgi.py
+++ src/rfdoc/rfdoc_wsgi.py
@@ -6,12 +6,26 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
+import site
 import sys
 
-THIS_PATH = os.path.join(os.path.dirname(__file__))
-sys.path.append(THIS_PATH)
-sys.path.append(os.path.join(THIS_PATH, '..'))
+# Site-packages reside inside the virtualenv
+SITE_PACKAGES_PATH = os.path.join(os.path.expanduser('~'), 'venv', 'lib',
+    'python2.7', 'site-packages')
+
+# Adds site-packages to the PATH
+prev_sys_path = list(sys.path)
+site.addsitedir(SITE_PACKAGES_PATH)
+new_sys_path = []
+for item in list(sys.path):
+    if item not in prev_sys_path:
+        new_sys_path.append(item)
+        sys.path.remove(item)
+sys.path[:0] = new_sys_path
+
+# Defines the name of the settings module
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rfdoc.settings")
 
+# Initializes WSGI
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
