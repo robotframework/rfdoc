b8c5d43902d3906c69fc098c1d0d19b661aa82ce
Janne Piironen <janne.piironen@gmail.com>
2014-03-27 14:27:53 +0200

    Minor UI improvements. Wrote help for search options.

--- src/rfdoc/rfdocapp/static/rfdoc.js
+++ src/rfdoc/rfdocapp/static/rfdoc.js
@@ -2,4 +2,8 @@ $(document).ready(function() {
     $('.versions-current').click(function(){
         $('#versions dl').toggle();
     });
+
+    $('a.help').click(function(){
+       $('div.help').toggle();
+    });
 });
